e6ec88212ee098fcfaaed2c331d188501bb8f794
antonitsm <antonitsm30@gmail.com>
2025-08-06 21:09:51 -0300

    integração parcial das paginas

---
+++ landing/static/landing/js/home.js
@@ -0,0 +1,7 @@
+document.getElementById('cadastrar').addEventListener('click', function() {
+    alert('Cadastro');
+});
+
+document.getElementById('cadastrar2').addEventListener('click', function() {
+    alert('Cadastro');
+});
