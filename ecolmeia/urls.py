f38f3d991d23a805ccff0636f4d9bd8ddcafc2f9
antonitsm <antonitsm30@gmail.com>
2025-08-10 16:51:53 -0300

    linkado as rotas de login e cadastro

--- ecolmeia/urls.py
+++ ecolmeia/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("landing.urls")),
     path("admin/", admin.site.urls),
-    path("login/", include("accounts.urls")),
+    path("conta/", include("accounts.urls")),
+    
 ]
