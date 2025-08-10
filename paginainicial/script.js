21b8bbbba430ef6b98e8aeb03d48f46999651662
sophiaMileski <sophiamileskisantos@gmail.com>
2025-08-09 21:12:48 -0300

    mudança na pagina inicial

--- paginainicial/script.js
+++ paginainicial/script.js
@@ -1,6 +1,5 @@
 console.log("Header carregado!");
 
-// Redirecionar para cadastro.html ao clicar no botão de cadastro
 document.getElementById('cadastro2')?.addEventListener('click', function() {
     window.location.href = "loginP2.html";
 });
