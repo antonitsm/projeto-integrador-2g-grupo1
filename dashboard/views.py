71e83311643e96e6f47e1ab2365dc51c9e631e37
Felipe Bordignon <felipebordignon44@gmail.com>
2025-08-21 13:12:19 -0300

    Eu exclui essa linha sem querer.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 def paginainicialdashboard_view(request):
     return render(request, 'dashboard/paginainicialdashboard.html')
 
