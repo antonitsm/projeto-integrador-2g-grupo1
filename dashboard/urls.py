47c15970bf80fd238d6c75a7e44bd6d0aaafe1bc
PietroMartiori <pietromartiori7@gmail.com>
2025-08-21 20:12:08 -0300

    REVERT DO REVERT DO COMMIT ERRADO
    confusao confusa entre os membros da equipe

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
@@ -7,4 +6,5 @@ urlpatterns = [
     path("dados/", views.dados_view, name="dados"),
     path("observacao/", views.observacao_view, name="observacao"),
     path("producao/", views.producao_view, name="producao"),
+    path("registros/", views.tudo_registros_view, name="tudo_registros"),
 ]
