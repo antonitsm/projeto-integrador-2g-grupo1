431fbd5e8f02c183b42fad38eff8816e23c595cf
Felipe Bordignon <felipebordignon44@gmail.com>
2025-08-22 14:56:39 -0300

    página de vizualição das colmeias
    Fiz a página de vizualizar as colmeias, está praticamente pronta agora falta a parte com a integração do back-end.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,4 +7,15 @@ urlpatterns = [
     path("observacao/", views.observacao_view, name="observacao"),
     path("producao/", views.producao_view, name="producao"),
     path("registros/", views.tudo_registros_view, name="tudo_registros"),
+
+    # URLs para Registros
+    path("registros/detalhes/<int:registro_id>/", views.detalhes_registro_ajax, name="detalhes_registro"),
+    path("registros/novo/", views.novo_registro_view, name="novo_registro"),
+
+    # URLs para Colmeias
+    path("minhas_colmeias/", views.minhas_colmeias_view, name="minhas_colmeias"),
+    path("colmeias/nova/", views.nova_colmeia_view, name="nova_colmeia"),
+    path("colmeias/editar/<int:colmeia_id>/", views.editar_colmeia_view, name="editar_colmeia"),
+    path("colmeias/producao/<int:colmeia_id>/", views.editar_producao_view, name="editar_producao"),
+    path("colmeias/registro/<int:colmeia_id>/", views.adicionar_registro_view, name="adicionar_registro"),
 ]
