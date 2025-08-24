db4579049e2d95b0c2cba6f2ec3d1aad73635aa7
Sophia Mileski <sophiamileskisantos@gmail.com>
2025-08-24 18:09:07 -0300

    linkagem mudada novamente(agora vai)

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.login_view, name="login"),
-    path("cadastro/", views.cadastro_view, name="cadastro"),
+    path("cadastro/", views.cadastro_view, name="accounts:cadastro"),
     path("minha-conta/", views.conta_view, name="conta"),
     path('', views.paginainicialdashboard_view, name='paginainicialdashboard'),
     path('cadastro/', views.cadastro_view, name='cadastro'),
-    path('login/', views.login_view, name='login'),
+    path('login/', views.login_view, name='accounts:login'),
 ]
