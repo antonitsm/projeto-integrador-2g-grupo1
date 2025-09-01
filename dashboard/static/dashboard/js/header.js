a31798cd25a2ddfe5f227005b5b711474ae31332
Sophia Mileski <sophiamileskisantos@gmail.com>
2025-09-01 20:30:59 -0300

    responsividade do header feita, css de produção mudado pra responsividade, e css do header alteradol pra responsividade

--- dashboard/static/dashboard/js/header.js
+++ dashboard/static/dashboard/js/header.js
@@ -2,3 +2,11 @@
 //  const menu = document.querySelector('.navbar');
 //  menu.classList.toggle('active');
 //}
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("mobile-toggle");
+    const navbar = document.getElementById("navbar");
+
+    toggle.addEventListener("click", () => {
+      navbar.classList.toggle("active");
+    });
+  });
